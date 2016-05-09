9dc3ac7bf5d4a8789461a10f29113e5192f12552
Abdullah Adeeb <contact@abdullahadeeb.com>
2016-05-09 00:11:18 -0400

    added gitignore to repo

--- app/scripts/angularfire/auth.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-  angular.module('firebase.auth', ['firebase', 'firebase.ref'])
-    .factory('Auth', function($firebaseAuth, FireRef) {
-      return $firebaseAuth(FireRef);
-    });
-})();
--- app/scripts/angularfire/config.js
+++
@@ -1,4 +0,0 @@
-angular.module('firebase.config', [])
-  .constant('FBURL', 'https://luminous-torch-8131.firebaseio.com')
-  .constant('SIMPLE_LOGIN_PROVIDERS', ['password'])
-  .constant('loginRedirectPath', '/login');
